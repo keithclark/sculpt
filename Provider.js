5ae6b55e043323010f784f44c58a11e931b50b6d
Keith Clark <keith@keithclark.co.uk>
2020-03-19 20:16:17 +0000

    Throw error when extended class don’t implement base methods

--- Provider.js
+++ Provider.js
@@ -1,17 +1,21 @@
+const notImplemented = method => {
+  throw new Error(`Provider.${method}() - method not implemented`);
+}
+
 module.exports = class Provider {
   find() {
-
+    notImplemented('find');
   }
 
   create() {
-
+    notImplemented('create');
   }
 
   update() {
-
+    notImplemented('update');
   }
 
   delete() {
-
+    notImplemented('delete');
   }
 }
